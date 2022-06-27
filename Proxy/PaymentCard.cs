7db7f3fb8915544e8d80a1fb564f692deeb22669
KMV <karl-martin.vogel@tc.de>
2022-06-27 12:32:46 +0200

    Add project files.

---
+++ Proxy/PaymentCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class PaymentCard
+    {
+        Accounts? AccountA { get; } // um beliebig viele Accounts erweitern?
+        Accounts? AccountB { get; }
+
+        public PaymentCard(Accounts? accountA, Accounts? accountB)
+        {
+            this.AccountA = accountA;
+            this.AccountB = accountB;
+        }
+
+        public void Pay(double price)
+        {
+            Console.WriteLine("Kontostand Konto A: "+AccountA.Balance + "\n" +
+                "Kontostand Konto B: "+AccountB.Balance);
+            double percentsAccountA = AccountA.Balance/(AccountA.Balance + AccountB.Balance);
+            double percentsAccountB = AccountB.Balance / (AccountA.Balance + AccountB.Balance);
+            double paidMoney = percentsAccountA * price + percentsAccountB * price;
+            AccountA.Balance = AccountA.Balance - percentsAccountA * price;
+            AccountB.Balance = AccountB.Balance - percentsAccountB * price;
+            Console.WriteLine("bezahlt: "+paidMoney);
+            Console.WriteLine("Kontostand Konto A: " + AccountA.Balance + "\n" +
+                "Kontostand Konto B: " + AccountB.Balance);
+
+        }
+
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Proxy
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Accounts accountA = new Accounts();
+            Accounts accountB = new Accounts();
+            Random rnd = new Random();
+            accountA.Balance = rnd.NextDouble() * 1000 + rnd.NextDouble() * 10;
+            accountB.Balance = rnd.NextDouble() * 1000 + rnd.NextDouble() * 10;
+            PaymentCard KreditKarte = new PaymentCard(accountA, accountB);
+
+            // set AccountBalances
+
+            Console.WriteLine("Bitte Produktpreis eingeben!");
+            string input = Console.ReadLine();
+            double price = Convert.ToDouble(input);
+
+            Console.WriteLine("wird bezahlt mit Kreditkarte...");
+            Thread.Sleep(1000);
+
+            KreditKarte.Pay(price);
+
+
+        }
+    }
+}
